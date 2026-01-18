56cf8c36cacd8b6b97199ea81f343919ba69b375
Jonathan Alvindo Fernandi <jonathanafernandi@gmail.com>
2026-01-18 23:34:16 +0700

    Fix bugs

--- routes/web.php
+++ routes/web.php
@@ -8,25 +8,15 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\WishlistController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [BookController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
+
+Route::middleware(['auth', 'role:admin'])->group(function() {
+    Route::resource('books', BookController::class)->except(['index', 'show']);
 });
-
-// TODO: Middleware route for admin-only pages
-// Route::middleware(['auth', 'role:admin'])->group(function() {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// })
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
 
-    Route::resource('books', BookController::class)
-        ->except(['index', 'show']);
+    Route::resource('books', BookController::class)->only(['index', 'show']);
 
     Route::post('/books/{book}/borrow', [BorrowingController::class, 'store'])
         ->name('books.borrow');
@@ -47,14 +37,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-//Route::get('/', [BookController::class, 'index']);
-//Route::resource('books', BookController::class);
-
-//Route::post('/books/{book}/borrow', [BorrowingController::class, 'store'])
-//    ->name('books.borrow');
-
-//Route::post('/borrowings/{borrowing}/return', [BorrowingController::class, 'return'])
-//    ->name('borrowings.return');
 
 Route::get('locale/{lang}', [LocaleController::class, 'setLocale']);
